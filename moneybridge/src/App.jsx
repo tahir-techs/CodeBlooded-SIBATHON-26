70124f98f109a65d2a4997444a42c3dcac859221
deepaklal009 <deepak.doultani00@gmail.com>
2026-02-13 15:51:59 +0500

    react setup

---
+++ moneybridge/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-black">
+      <h1 className="text-4xl font-bold text-green-400">
+        Tailwind Working 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
+  
